0f412eca9818f14c80ba86deff993663f41b8c16
Gwachukwu Elebeke <55688650+Gwachukwu@users.noreply.github.com>
2021-01-20 12:02:16 +0100

    Bug fix

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -2,21 +2,27 @@ const axios = require("axios");
 
 exports.requestRates = async (req, res, next) => {
   const { base, currency } = req.query;
+
+  //validation
   if (!base || !currency) {
     return res.status(400).json({
       status: 400,
       message: "Please provide the base and currency parameters",
     });
   }
+
   try {
+    //api request using axios
     const exchangeRates = await axios.get(
       `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${currency}`
     );
     if (exchangeRates) {
+      //return result
       return res.status(200).json({
         results: exchangeRates.data,
       });
     } else {
+      //return error on failure
       return res.status(500).json({
         status: 500,
         results: "something went wrong, please try again later",
